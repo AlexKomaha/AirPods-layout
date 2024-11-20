9961fb3419920199ed6cbb329e5c9c18e6b58dcd
AlexKomaha <107383667+AlexKomaha@users.noreply.github.com>
2024-11-20 17:12:52 +0900

    Added opacity and js for advantages-btn

--- js/product-page.js
+++ js/product-page.js
@@ -9,9 +9,29 @@ document.addEventListener("DOMContentLoaded", function() {
   function updateSlider() {
     const offset = -cardWidth * currentIndex;
     slider.style.transform = `translateX(${offset}px)`;
-  }
 
-  leftBtn.addEventListener("click", () => {
+
+  // Manage button classes
+
+  if (currentIndex === 0) {
+    leftBtn.classList.add("inactive");
+    rightBtn.classList.remove("inactive");
+    leftBtn.classList.remove("active");
+    rightBtn.classList.add("active");
+  } else if (currentIndex === slider.children.length - 3) {
+    leftBtn.classList.remove("inactive");
+    rightBtn.classList.add("inactive");
+    leftBtn.classList.add("active");
+    rightBtn.classList.remove("active");
+  } else {
+    leftBtn.classList.remove("inactive");
+    rightBtn.classList.remove("inactive");
+    leftBtn.classList.add("active");
+    rightBtn.classList.add("active");
+  }
+}
+
+leftBtn.addEventListener("click", () => {
     if (currentIndex > 0) {
       currentIndex--;
       updateSlider();
